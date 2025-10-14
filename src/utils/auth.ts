ae960a7434ae009082993151ceef6016e2d63f85
Biswajit Debnath <biswajitdebnath405@gmail.com>
2025-10-14 17:18:36 +0530

    Revert "fix build errors, setup vercel, add deployment docs"

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,24 +1,16 @@
 import { auth } from '@clerk/nextjs/server';
 import { prisma } from "./db";
 
+
 export const getUserByClerkId = async () => {
-    try {
-        const { userId } = await auth();
-        
-        // If no userId, throw error
-        if (!userId) {
-            throw new Error('No user ID found');
+
+    const { userId } = await auth();
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            clerkId : userId
         }
+    });
 
-        const user = await prisma.user.findUniqueOrThrow({
-            where: {
-                clerkId : userId
-            }
-        });
-
-        return user;
-    } catch (error) {
-        console.log('Auth error:', error);
-        throw error;
-    }
+    return user;
 }
